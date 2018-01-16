fe3929c10e7adfb613bcd42a117109c19b4bd361
Ayneko <BlackCaatt@mail.ru>
2018-01-16 17:30:07 +0000

    5 groups

--- 5p/routes/index.js
+++ 5p/routes/index.js
@@ -7,3 +7,24 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+/* Страница imagine dragons */
+router.get("/ig",function(req,res,next){
+ res.send("<h1>Страница Imagine Dragons</h1>")
+})
+/* Страница muse */
+router.get("/muse",function(req,res,next){
+ res.send("<h1>Страница Muse</h1>")
+})
+/* Страница Placebo */
+router.get("/placebo",function(req,res,next){
+ res.send("<h1>Страница Placebo</h1>")
+})
+/* Страница 21 pilots */
+router.get("/21p",function(req,res,next){
+ res.send("<h1>Страница Twenty one pilots</h1>")
+})
+/* Страница Artic Monkeys */
+router.get("/am",function(req,res,next){
+ res.send("<h1>Страница Совы</h1>")
+})
