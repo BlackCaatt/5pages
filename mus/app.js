07134abb8917fa781c651f3534e4d4124bab55f9
Ayneko <BlackCaatt@mail.ru>
2018-01-16 21:17:48 +0000

    with bootsrap

--- mus/app.js
+++ mus/app.js
@@ -12,6 +12,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -22,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'bower_components')));
 
 app.use('/', index);
 app.use('/users', users);
